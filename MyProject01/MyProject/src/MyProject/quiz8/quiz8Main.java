c23f422f67b379aaec4f17e7d4bdbc3df6b47591
himediaEUNWOOCHA <vltfhem1206@gmail.com>
2023-03-31 17:32:50 +0900

    20230331인터페이스 추상메소드

--- MyProject01/MyProject/src/MyProject/quiz8/quiz8Main.java
+++ MyProject01/MyProject/src/MyProject/quiz8/quiz8Main.java
@@ -4,13 +4,13 @@ public class quiz8Main {
 
 	public static void main(String[] args) {
 		
-		quiz8Class q8 = new quiz8Class();
-		q8.showData(q8.add(10, 11), '0');
-		q8.showData(q8.substract(57, 47), '1');
-		q8.showData(q8.multiply(22, 10), '2');
-		q8.showData(q8.divide(5.8, 6.7));
+		quiz8_Calulator q8 = new quiz8Class();
+		((quiz8Class) q8).showData(q8.add(10, 11), '0');
+		((quiz8Class) q8).showData(q8.substract(57, 47), '1');
+		((quiz8Class) q8).showData(q8.multiply(22, 10), '2');
+		((quiz8Class) q8).showData(q8.divide(5.8, 6.7));
 		
-		quiz8_Calulator q8Interface = new quiz8Class();
+//		quiz8_Calulator q8Interface = new quiz8Class();
 		
 		
 	}
